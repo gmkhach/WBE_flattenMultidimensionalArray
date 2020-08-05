8a746517135b9515a8d2341107a2507dcb2357bf
Gabriel Khachatryan <gmkhach@gmail.com>
2020-08-05 03:25:00 -0400

    using reduce to flatten the array

--- flattenMultidimensionalArray.js
+++ flattenMultidimensionalArray.js
@@ -1,5 +1,5 @@
- 
-function flatten(input) {
+// Brute force method 
+function flattenBrute(input) {
     let result = [];
 
     input.forEach(arr => {
@@ -10,6 +10,22 @@ function flatten(input) {
 }
 
 let mdArray = [[1, 2], [3, 4, 5, 6], [7, 8, 9]];
+console.log(flattenBrute(mdArray));
+
+mdArray = [['a', 'b', 'c', 'd'], ['e', 'f'], ['g'], ['g', 'i', 'j', 'k', 'l', 'm'], ['n', 'o'], ['p']];
+console.log(flattenBrute(mdArray));
+
+mdArray = [['a', 'b', 'c', 'd'], [1, 2, 3, 4], [{}, {i: Math.sqrt(-1)}]];
+console.log(flattenBrute(mdArray));
+
+// Now implement it using the built-in reduce method
+function flatten(input) {
+    return input.reduce((result, arr) => {
+        return result.concat(arr);
+    })    
+} 
+
+mdArray = [[1, 2], [3, 4, 5, 6], [7, 8, 9]];
 console.log(flatten(mdArray));
 
 mdArray = [['a', 'b', 'c', 'd'], ['e', 'f'], ['g'], ['g', 'i', 'j', 'k', 'l', 'm'], ['n', 'o'], ['p']];
